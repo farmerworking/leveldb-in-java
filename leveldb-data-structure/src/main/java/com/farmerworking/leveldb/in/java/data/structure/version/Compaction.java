720a825fc158c5462513b3059f23123606453044
farmerworking <farmerworking@gmail.com>
2019-07-01 18:07:21 +0800

    Compaction

---
+++ leveldb-data-structure/src/main/java/com/farmerworking/leveldb/in/java/data/structure/version/Compaction.java
@@ -0,0 +1,166 @@
+package com.farmerworking.leveldb.in.java.data.structure.version;
+
+import com.farmerworking.leveldb.in.java.api.Comparator;
+import com.farmerworking.leveldb.in.java.api.Options;
+import com.farmerworking.leveldb.in.java.data.structure.memory.InternalKey;
+import com.farmerworking.leveldb.in.java.data.structure.memory.InternalKeyComparator;
+
+import java.util.Vector;
+
+// A Compaction encapsulates information about a compaction.
+public class Compaction {
+    private final int level;
+
+    private long maxOutputFileSize;
+    private Version inputVersion;
+
+    // Index in grandparent_starts_
+    int grandparentIndex;
+
+    // Some output key has been seen
+    private boolean seenKey;
+
+    // Bytes of overlap between current output and grandparent files
+    private long overlappedBytes;
+
+    // level_ptrs_ holds indices into input_version_->levels_: our state
+    // is that we are positioned at one of the file ranges for each
+    // higher level than the ones involved in this compaction (i.e. for
+    // all L >= level_ + 2).
+    int[] levelPtrs = new int[Config.kNumLevels];
+
+    private VersionEdit edit;
+
+    // Each compaction reads inputs from "level_" and "level_+1"
+    Vector<FileMetaData>[] inputs = new Vector[2];
+
+    // State used to check for number of of overlapping grandparent files
+    // (parent == level_ + 1, grandparent == level_ + 2)
+    Vector<FileMetaData> grandparents;
+
+    public Compaction(Options options, int level) {
+        this.level = level;
+        this.maxOutputFileSize = maxFileSizeForLevel(options, level);
+        this.inputVersion = null;
+        this.grandparentIndex = 0;
+        this.seenKey = false;
+        this.overlappedBytes = 0;
+
+        for (int i = 0; i < Config.kNumLevels; i++) {
+            levelPtrs[i] = 0;
+        }
+    }
+
+    // Return the level that is being compacted.  Inputs from "level"
+    // and "level+1" will be merged to produce a set of "level+1" files.
+    public int getLevel() {
+        return level;
+    }
+
+    // Return the object that holds the edits to the descriptor done
+    // by this compaction.
+    public VersionEdit getEdit() {
+        return edit;
+    }
+
+    // "which" must be either 0 or 1
+    public int numInputFiles(int which) {
+        return inputs[which].size();
+    }
+
+    // Return the ith input file at "level()+which" ("which" must be 0 or 1).
+    public FileMetaData input(int which, int i) {
+        return inputs[which].get(i);
+    }
+
+    // Maximum size of files to build during this compaction.
+    public long maxOutputFileSize() {
+        return maxOutputFileSize;
+    }
+
+    // Is this a trivial compaction that can be implemented by just
+    // moving a single input file to the next level (no merging or splitting)
+    public boolean isTrivialMove() {
+        // Avoid a move if there is lots of overlapping grandparent data.
+        // Otherwise, the move could create a parent file that will require
+        // a very expensive merge later on.
+        return (this.numInputFiles(0) == 1 &&
+                this.numInputFiles(1) == 0 &&
+                totalFileSize(this.grandparents) <=
+                        maxGrandParentOverlapBytes(inputVersion.versionSetBelongTo.getOptions()));
+    }
+
+    // Add all inputs to this compaction as delete operations to *edit.
+    public void addInputDeletions(VersionEdit edit) {
+        for (int which = 0; which < 2; which++) {
+            for (int i = 0; i < inputs[which].size(); i++) {
+                edit.deleteFile(this.level + which, inputs[which].get(i).getFileNumber());
+            }
+        }
+    }
+
+    // Returns true if the information we have available guarantees that
+    // the compaction is producing data in "level+1" for which no data exists
+    // in levels greater than "level+1".
+    public boolean IsBaseLevelForKey(String userKey) {
+        // Maybe use binary search to find right entry instead of linear search?
+        char[] userKeyChar = userKey.toCharArray();
+        Comparator userComparator = inputVersion.versionSetBelongTo.getInternalKeyComparator().getUserComparator();
+        for (int lvl = this.level + 2; lvl < Config.kNumLevels; lvl++) {
+            Vector<FileMetaData> files = inputVersion.files.get(lvl);
+            for (; this.levelPtrs[lvl] < files.size();) {
+                FileMetaData metaData = files.get(this.levelPtrs[lvl]);
+                if (userComparator.compare(userKeyChar, metaData.getLargest().userKeyChar) <= 0) {
+                    // We've advanced far enough
+                    if (userComparator.compare(userKeyChar, metaData.getSmallest().userKeyChar) >= 0) {
+                        // Key falls in this file's range, so definitely not base level
+                        return false;
+                    }
+                    break;
+                }
+                this.levelPtrs[lvl]++;
+            }
+        }
+        return true;
+    }
+
+    // Returns true iff we should stop building the current output before processing "internal_key".
+    public boolean shouldStopBefore(InternalKey internalKey) {
+        InternalKeyComparator comparator = inputVersion.versionSetBelongTo.getInternalKeyComparator();
+        while(grandparentIndex < grandparents.size() &&
+                comparator.compare(internalKey, grandparents.get(grandparentIndex).getLargest()) > 0) {
+            if (this.seenKey) {
+                this.overlappedBytes += grandparents.get(grandparentIndex).getFileSize();
+            }
+            grandparentIndex ++;
+        }
+
+        this.seenKey = true;
+        if (this.overlappedBytes > maxFileSizeForLevel(inputVersion.versionSetBelongTo.getOptions(), level)) {
+            // Too much overlap for current output; start new output
+            this.overlappedBytes = 0;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private long maxFileSizeForLevel(Options options, int level) {
+        // We could vary per level to reduce number of files?
+        return targetFileSize(options);
+    }
+
+    protected long targetFileSize(Options options) {
+        return options.getMaxFileSize();
+    }
+
+    protected long totalFileSize(Vector<FileMetaData> files) {
+        return files.stream().mapToLong(FileMetaData::getFileSize).sum();
+    }
+
+    // Maximum bytes of overlaps in grandparent (i.e., level+2) before we
+    // stop building a single file in a level->level+1 compaction.
+    private long maxGrandParentOverlapBytes(Options options) {
+        return 10 * targetFileSize(options);
+    }
+}
